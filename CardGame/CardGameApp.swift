ae7741d1577497f870dd862b3f656fb18ed248ac
KyleBing(MBP14) <kylebing@163.com>
2024-03-08 09:54:42 +0800

    Initial Commit

---
+++ CardGame/CardGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardGameApp.swift
+//  CardGame
+//
+//  Created by Kyle on 2024/3/8.
+//
+
+import SwiftUI
+
+@main
+struct CardGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CardGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CardGame
+//
+//  Created by Kyle on 2024/3/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
