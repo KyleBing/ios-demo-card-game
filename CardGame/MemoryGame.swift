5d2d623826bd1b01f084038da6cdbe20f7329cc1
KyleBing(MBP14) <kylebing@163.com>
2024-03-18 22:55:19 +0800

    完成算法

--- CardGame/MemoryGame.swift
+++ CardGame/MemoryGame.swift
@@ -20,8 +20,37 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
         }
     }
     
-    func chooseCard(_ card: Card){
-        print(card)
+    // 最后一张向上卡片的 index
+    private var indexOfTheOneAndOnlyFaceUpCard: Int? {
+        get {
+            cards.indices
+                .filter{ !cards[$0].isMatched }  // 过滤掉已经匹配的
+                .filter{ cards[$0].isFaceUp }.only
+        }
+        set {
+            cards.indices
+                .filter({ !cards[$0].isMatched }) // 过滤掉已经匹配的
+                .forEach{  cards[$0].isFaceUp = (newValue == $0) }
+        }
+    }
+
+    
+    mutating func chooseCard(_ card: Card){
+        if let chosenIndex = cards.firstIndex(where: { $0.id == card.id }) {
+            // 卡片未朝上的 && 卡片未被匹配的才会动作它
+            if !cards[chosenIndex].isFaceUp && !cards[chosenIndex].isMatched{
+                if let lastIndexFaceUp = indexOfTheOneAndOnlyFaceUpCard{
+                    if cards[lastIndexFaceUp].content == cards[chosenIndex].content {
+                        // 如果匹配
+                        cards[lastIndexFaceUp].isMatched = true
+                        cards[chosenIndex].isMatched = true
+                    }
+                } else {
+                    indexOfTheOneAndOnlyFaceUpCard = chosenIndex
+                }
+                cards[chosenIndex].isFaceUp = true
+            }
+        }
     }
     
     mutating func shuffle(){
@@ -29,7 +58,7 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
     }
     
     struct Card: Equatable, Identifiable, CustomDebugStringConvertible{
-        var isFaceUp = true
+        var isFaceUp = false
         var isMatched = false
         var content: CardContent
         
@@ -37,5 +66,13 @@ struct MemoryGame<CardContent> where CardContent: Equatable{
         var debugDescription: String {
             "\(id)-\(content)-\(isMatched)-\(isFaceUp)"
         }
+    }
+}
+
+// MARK: - 扩展 Array
+
+extension Array{
+    var only: Element? {
+        count == 1 ? first : nil
     }
 }
